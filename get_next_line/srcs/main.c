ccf0d8da6c3c896286a8fc06e10fd96a88c4184d
Marre ALEXANDRE <malexand@e2r9p6.42.fr>
2016-11-18 17:34:48 +0100

    Send other timeout

--- get_next_line/srcs/main.c
+++ get_next_line/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: malexand <malexand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 14:55:12 by malexand          #+#    #+#             */
-/*   Updated: 2016/11/18 17:25:08 by malexand         ###   ########.fr       */
+/*   Updated: 2016/11/18 17:33:58 by malexand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,36 @@ int		main(int argc, const char **argv)
 	get_next_line(p[0], &line);
 	if (strcmp(line, str) == 0)
 		printf("TRUE\n");
+
+	// char *line;
+	// int fd;
+	// int fd2;
+	// int fd3;
+	// int	diff_file_size;
+    
+ 	// system("mkdir -p sandbox");
+	// system("openssl rand -base64 $((30 * 1000 * 3/4)) | tr -d '\n' | tr -d '\r' > sandbox/one_big_fat_line.txt");
+	// system("echo '\n' >> sandbox/one_big_fat_line.txt");
+
+	// fd = open("sandbox/one_big_fat_line.txt", O_RDONLY);
+	// fd2 = open("sandbox/one_big_fat_line.txt.mine", O_CREAT | O_RDWR | O_TRUNC, 0755);
+
+	// while (get_next_line(fd, &line) == 1)
+	// {
+	//     write(fd2, line, strlen(line));
+	//     write(fd2, "\n", 1);
+	// }
+	// if (line)
+	// 	write(fd2, line, strlen(line));
+	// close(fd);
+	// close(fd2);
+
+	// system("diff sandbox/one_big_fat_line.txt sandbox/one_big_fat_line.txt.mine > sandbox/one_big_fat_line.diff");
+	// fd3 = open("sandbox/one_big_fat_line.diff", O_RDONLY);
+	// diff_file_size = read(fd3, NULL, 10);
+	// close(fd3);
+
+	// if (diff_file_size == 0)
+	//	printf("TRUE\n");
 	return (0);
 }
