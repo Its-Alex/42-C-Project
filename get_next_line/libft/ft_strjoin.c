5b94921e578fbb551cb298526e88f014d44befe8
malexand <malexand@student.42.fr>
2016-11-23 12:22:40 +0100

    patch strhoin

--- get_next_line/libft/ft_strjoin.c
+++ get_next_line/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: malexand <malexand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/07 21:05:39 by alex              #+#    #+#             */
-/*   Updated: 2016/11/22 15:14:09 by malexand         ###   ########.fr       */
+/*   Updated: 2016/11/23 12:21:02 by malexand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	size_t	len_s2;
 	char	*str;
 
-	lens_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
 	if (!s1 || !s2)
 		return (NULL);
+	lens_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
 	str = ft_strnew(sizeof(char) * (lens_s1 + len_s2));
 	if (str == NULL)
 		return (NULL);
