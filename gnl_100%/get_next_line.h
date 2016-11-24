efca3f7050a6399938274c7324f0f705181ebbe9
malexand <malexand@student.42.fr>
2016-11-24 12:13:28 +0100

    Send right gnl

---
+++ gnl_100%/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vacrozet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/16 11:18:01 by vacrozet          #+#    #+#             */
+/*   Updated: 2016/11/23 10:27:39 by vacrozet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# define BUFF_SIZE 8
+# define FD_SIZE 2147483647
+
+int		get_next_line(const int fd, char **line);
+
+#endif
