d94ee767b8f8afe1e6f584577c7773e4279d1763
malexand <malexand@student.42.fr>
2017-02-10 16:29:21 +0100

    Patch norm move.c

--- wolf3d/srcs/move.c
+++ wolf3d/srcs/move.c
@@ -6,7 +6,7 @@
 /*   By: malexand <malexand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/10 13:28:29 by malexand          #+#    #+#             */
-/*   Updated: 2017/02/10 15:53:21 by malexand         ###   ########.fr       */
+/*   Updated: 2017/02/10 16:28:34 by malexand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,25 @@ static void		run(t_env *e)
 	speed = 0.04;
 	if (e->key.run == 1)
 	{
-		if(e->map->mapgen[(int)(e->persp->posx + e->persp->dirx * speed)]
+		if (e->map->mapgen[(int)(e->persp->posx + e->persp->dirx * speed)]
 				[(int)(e->persp->posy)] != 1)
 			e->persp->posx += e->persp->dirx * speed;
-    	if(e->map->mapgen[(int)(e->persp->posx)]
-    			[(int)(e->persp->posy + e->persp->diry * speed)] != 1)
-    		e->persp->posy += e->persp->diry * speed;
-
+		if (e->map->mapgen[(int)(e->persp->posx)]
+				[(int)(e->persp->posy + e->persp->diry * speed)] != 1)
+			e->persp->posy += e->persp->diry * speed;
 	}
 	if (e->key.run == -1)
 	{
-		if(e->map->mapgen[(int)(e->persp->posx - e->persp->dirx * speed)]
+		if (e->map->mapgen[(int)(e->persp->posx - e->persp->dirx * speed)]
 				[(int)(e->persp->posy)] != 1)
 			e->persp->posx -= e->persp->dirx * speed;
-    	if(e->map->mapgen[(int)(e->persp->posx)]
-    			[(int)(e->persp->posy - e->persp->diry * speed)] != 1)
-    		e->persp->posy -= e->persp->diry * speed;
+		if (e->map->mapgen[(int)(e->persp->posx)]
+				[(int)(e->persp->posy - e->persp->diry * speed)] != 1)
+			e->persp->posy -= e->persp->diry * speed;
 	}
 }
 
-void	key_move(t_env *e)
+void			key_move(t_env *e)
 {
 	run(e);
 	if (e->key.turn == 1)
