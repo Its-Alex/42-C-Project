93324570d402e501407d210b71c47eff428a1191
Alexandre MARRE <xSkyZie@gmail.com>
2017-11-28 15:47:14 +0100

    Focus on parse

--- ComputorV1/ComputorV1.go
+++ ComputorV1/ComputorV1.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func findFromX(str *string, index int) []int {
-	begin, end := 0, index+3
-
-	for ; (*str)[index] >= '0' && (*str)[index] <= '9'; index-- {
-		begin = index
+func findFromX(str string) (int, int, int) {
+	posx := make([]int, 3)
+	for pow := 0; pow < 3; pow++ {
+		pos := strings.Index(str, "*x^"+strconv.Itoa(pow))
+		if pos != -1 {
+			posx[pow] = 0
+		} else {
+			for count := pos; str[count] >= '0' && str[count] >= '9' && count >= 0; count-- {
+				fmt.Println(str[count])
+			}
+		}
 	}
-	fmt.Println(begin, end)
-	fmt.Println((*str)[begin:end])
-	return make([]int, 6)
+	return posx[0], posx[1], posx[2]
 }
 
 func parseEqua(equa string) []int {
@@ -23,26 +28,25 @@ func parseEqua(equa string) []int {
 	str := strings.Replace(equa, " ", "", -1)
 	str = strings.ToLower(str)
 	s := strings.Split(str, "=")
-	findFromX(&s[0], strings.Index(s[0], "x^1"))
-	fmt.Printf("%c\n", s[0][6])
-	fmt.Println(strings.Index(s[0], "x^1"))
+	value[0], value[1], value[2] = findFromX(s[0])
+	value[3], value[4], value[5] = findFromX(s[1])
+	fmt.Println(s)
 
 	return value
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Need to pass equation like this: ./ComputorV1 \"5X^1 = 0\"")
+	if len(os.Args) != 2 {
+		fmt.Println("Need to pass equation like this: ./ComputorV1 \"5 * X^1 = 0\"")
 		os.Exit(0)
 	}
-	equa := os.Args[1:2][0]
-
-	if equa == "" || strings.Index(equa, "=") == -1 {
-		fmt.Println("Equation not well formated")
+	equ := strings.Split(os.Args[1], "=")
+	if len(equ) != 2 {
+		fmt.Println("os.Args[1]tion not well formated")
 		os.Exit(0)
 	}
 
-	parsed := parseEqua(equa)
+	parsed := parseEqua(os.Args[1])
 	fmt.Println(parsed)
 	// for index := 0; index < len(split); index++ {
 	// 	fmt.Println(split[index])
