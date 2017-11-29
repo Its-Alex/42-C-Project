0a52a7481f8aee2f4fd1e4f4930442eb213e5fd5
Alexandre MARRE <xSkyZie@gmail.com>
2017-11-29 17:12:58 +0100

    Basic parse done

--- ComputorV1/ComputorV1.go
+++ ComputorV1/ComputorV1.go
@@ -3,36 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
-func findFromX(str string) (int, int, int) {
-	posx := make([]int, 3)
-	for pow := 0; pow < 3; pow++ {
-		pos := strings.Index(str, "*x^"+strconv.Itoa(pow))
-		if pos != -1 {
-			posx[pow] = 0
-		} else {
-			for count := pos; str[count] >= '0' && str[count] >= '9' && count >= 0; count-- {
-				fmt.Println(str[count])
-			}
+func getValue(data [][]string) []float64 {
+	var pow int
+	var value float64
+	var tab []float64
+	for count := 0; count < len(data); count++ {
+		pow, _ = strconv.Atoi(data[count][2])
+		for len(tab) <= pow {
+			tab = append(tab, 0)
 		}
+		value, _ = strconv.ParseFloat(data[count][1], 64)
+		tab[pow] += value
 	}
-	return posx[0], posx[1], posx[2]
-}
-
-func parseEqua(equa string) []int {
-	value := make([]int, 6)
-
-	str := strings.Replace(equa, " ", "", -1)
-	str = strings.ToLower(str)
-	s := strings.Split(str, "=")
-	value[0], value[1], value[2] = findFromX(s[0])
-	value[3], value[4], value[5] = findFromX(s[1])
-	fmt.Println(s)
-
-	return value
+	return tab
 }
 
 func main() {
@@ -40,15 +28,36 @@ func main() {
 		fmt.Println("Need to pass equation like this: ./ComputorV1 \"5 * X^1 = 0\"")
 		os.Exit(0)
 	}
-	equ := strings.Split(os.Args[1], "=")
-	if len(equ) != 2 {
-		fmt.Println("os.Args[1]tion not well formated")
+	equ := os.Args[1]
+	str := strings.Replace(equ, " ", "", -1)
+	str = strings.ToUpper(str)
+	s := strings.Split(str, "=")
+	if len(s) != 2 {
+		fmt.Println("Equation not well formated")
 		os.Exit(0)
 	}
 
-	parsed := parseEqua(os.Args[1])
-	fmt.Println(parsed)
-	// for index := 0; index < len(split); index++ {
-	// 	fmt.Println(split[index])
-	// }
+	re1, _ := regexp.Compile(`(\-?[0-9]+|\-?[0-9]+\.[0-9]+)\*X\^([0-9]+)`)
+	left, right := re1.FindAllStringSubmatch(s[0], -1), re1.FindAllStringSubmatch(s[1], -1)
+	var valueLeft, valueRight []float64
+	valueLeft = getValue(left)
+	valueRight = getValue(right)
+	for len(valueLeft) != len(valueRight) {
+		if len(valueLeft) < len(valueRight) {
+			valueLeft = append(valueLeft, 0)
+		} else {
+			valueRight = append(valueRight, 0)
+		}
+	}
+	fmt.Println(valueLeft)
+	fmt.Println(valueRight)
+	for index := 0; index < len(valueLeft); index++ {
+		valueLeft[index] -= valueRight[index]
+	}
+	if len(valueLeft) > 3 {
+		fmt.Println()
+	}
+	delta :=
+		fmt.Println(valueLeft)
+
 }
